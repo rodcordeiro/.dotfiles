7c1c2b00d0a951fd4720f7cea68a9f1ed3999a0d
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-09-28 21:55:41 -0300

    Update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,8 +21,23 @@
 
 Param()
 
-function SignScripts{
-<# 
+## Configs
+$console = $host.ui.rawui
+
+$console.BackgroundColor = "black"
+$console.foregroundcolor = "cyan"
+
+if ((New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+    $Title = "< GODMODE />"
+}
+else {
+    $Title = "< RodC0rdeiro />"
+}
+$console.WindowTitle = $Title
+
+
+function SignScripts {
+    <# 
 .SYNOPSIS 
     Sign a script with a certificate 
 .DESCRIPTION
@@ -36,45 +51,99 @@ function SignScripts{
 #> 
     
     param([parameter(ValueFromPipelineByPropertyName)][string]$Path)
-    if (!$Path){
-      Write-Host "You must pass the path argument"
-      Break
+    if (!$Path) {
+        Write-Host "You must pass the path argument"
+        Break
     }
     Write-Host ""
-      $certificates = Get-ChildItem cert:\LocalMachine\My
-      if(!$certificates){
-		return Write-Host "There's no available certificate. Please refer to https://guidooliveira.com/gerando-certificados-para-assinar-digitalmente-seus-scripts/ for instructions about creating sign certificate"
-	}
-      $counter = 1;
-      Write-Host "Bellow are the available certificates:"
-      $certificates | ForEach-Object {
+    $certificates = Get-ChildItem cert:\LocalMachine\My
+    if (!$certificates) {
+        return Write-Host "There's no available certificate. Please refer to https://guidooliveira.com/gerando-certificados-para-assinar-digitalmente-seus-scripts/ for instructions about creating sign certificate"
+    }
+    $counter = 1;
+    Write-Host "Bellow are the available certificates:"
+    $certificates | ForEach-Object {
         $cer = $_ | Select-Object Subject
         Write-Host "$counter | $cer"
         $counter = $counter + 1
-      }
-      Write-Host ""
+    }
+    Write-Host ""
       
-      $opt = $(Read-Host -Prompt "Enter the certificate number choosed") - 1
-      $certificate = $certificates[$opt]
+    $opt = $(Read-Host -Prompt "Enter the certificate number choosed") - 1
+    $certificate = $certificates[$opt]
       
-      Set-AuthenticodeSignature $Path $certificate
+    Set-AuthenticodeSignature $Path $certificate
 }
 
+function vscode {
+    <# 
+    .SYNOPSIS 
+        Opens VSCode with some customization
+    .DESCRIPTION
+        Opens VSCode with some customization
+    .Parameter <Path>
+        Path to folder to be opened
+    .Example
+        vscode ./example.ps1
+    .Example
+        vscode C:\Scripts\example.ps1
+    .Example
+        vscode
+    .Example
+        vscode .
+    #> 
+        
+    param([parameter(ValueFromPipelineByPropertyName)][string]$Path)
+        
+    if (!$Path) {
+        $Path = $PWD
+    }
+    if (Get-Command code-insiders) {
+        $code = $(get-command code-insiders).source
+    }
+    elseif (Get-Command code) {
+        $code = $(get-command code).source
+    }
+    else {
+        Write-Host "VSCode not installed. Please verify";
+        Break;
+    }
+    Start-Process -FilePath $code -ArgumentList "$(Resolve-Path -Path $Path)"
+        
+}
+
+function Prompt {
+    <# 
+    .SYNOPSIS 
+        Changes PS Prompt
+    .DESCRIPTION
+        Changes PS Prompt
+    #> 
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    $CmdPromptUser = [Security.Principal.WindowsIdentity]::GetCurrent();
+    $CmdPromptCurrentFolder = Split-Path -Path $pwd -Leaf
+    # Write-Host "$($CmdPromptUser.Name.split("\")[1]) " -ForegroundColor green -NoNewline
+    
+    if ($IsAdmin) {
+        $Title = "< GODMODE />"
+    }
+    else {
+        $Title = "< RodC0rdeiro />"
+    }
+    $host.ui.rawui.WindowTitle = $Title + "$(if($PWD.Path -eq $env:USERPROFILE){" ~HOME"} else {"$CmdPromptCurrentFolder"})"
+    
+    Write-Host "$CmdPromptCurrentFolder " -NoNewline
+    
+    if (Test-Path -Path .git) {
+        $CurrentBranch = git branch | select-string "\*"
+        Write-Host "($($CurrentBranch.ToString().split(" ")[1])) " -ForegroundColor cyan -NoNewline
+    }
+
+    Write-host ($(if ($IsAdmin) { '#' } else { '$' })) -NoNewline
+    return "> "    
+}
+    
 ## ALIASES
 Set-Alias sign SignScripts
-Set-Alias code code-insiders
-$projetos="C:\Users\$env:username\Projetos"
-
-## Configs
-$console = $host.ui.rawui
-If($True -eq ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){
-	## Admin configs
-	$console.backgroundcolor = "black"
-	$console.foregroundcolor = "cyan"
-	$console.WindowTitle = "Modo insano, ATIVAR!"
-} else {
-	## User configs
-	$console.backgroundcolor = "black"
-	$console.foregroundcolor = "cyan"
-	$console.WindowTitle = "RodCordeiro"
-}
+Set-Alias code vscode
+$projetos = "C:\Users\$env:username\Projetos"
