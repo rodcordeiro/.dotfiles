ac2e97058a06d84338262bc96de486db5f3b5e3d
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2026-05-05 16:18:09 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -162,6 +162,38 @@ function GetAllFiles {
   $items = @(Get-ChildItem -Hidden; Get-ChildItem)
   $items
 }
+
+function Digita {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromRemainingArguments, Position = 0)]
+        [string[]]$Text,
+
+        [switch]$SemEnter
+    )
+
+    $conteudo = ($Text -join ' ').Trim()
+    if ([string]::IsNullOrWhiteSpace($conteudo)) {
+        return
+    }
+
+    # adb input text usa %s para espaço
+    $conteudo = $conteudo -replace ' ', '%s'
+
+    & adb shell input text $conteudo
+
+    if (-not $SemEnter) {
+        & adb shell input keyevent 66
+    }
+}
+
+function Enter {
+    [CmdletBinding()]
+    param()
+
+    & adb shell input keyevent 66
+}
+
 function Auth {
   <#
 .SYNOPSIS
